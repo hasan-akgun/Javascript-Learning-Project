9672774181b5702f578521ec2f9f77186677919b
hasan-akgun <akgunhasan8@gmail.com>
2024-10-19 22:48:38 +0300

    Use modules and import from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-
+import {cart} from "../data/cart.js";
 
 let productsHTML='';
 
@@ -80,8 +80,23 @@ document.querySelectorAll('.js-add-to-cart')
     
             });
           }
-          console.log(cart);
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
         });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+          console.log(cart);
+          console.log(cartQuantity);
+
+        });
+
+        
+
+
         
         
   });
