182b9c5fb395b53875882448d269592057ffe917
hasan-akgun <akgunhasan8@gmail.com>
2025-01-30 16:23:49 +0300

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -27,7 +27,7 @@ cart.forEach((cartItem) => {
   });
 
   const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDates, 'days')
+  const deliveryDate = today.add(deliveryOption.deliveryDates, 'days');
   const dateString = deliveryDate.format('dddd, MMMM, D');
 
   cartSummaryHTML+=   `
@@ -84,7 +84,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
           <input type="radio"
             ${isChecked ? 'checked' : ''}
             class="delivery-option-input"
@@ -117,3 +117,11 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
     });
   });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=> {
+    element.addEventListener('click', () =>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    })
+  })
