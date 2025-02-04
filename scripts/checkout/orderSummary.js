7fa3150a7619ab584326bc27d0e555917cd13d18
hasan-akgun <akgunhasan8@gmail.com>
2025-02-04 21:07:37 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryoptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
 
@@ -106,6 +107,7 @@ export function renderOrderSummary(){
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove();
+        renderPaymentSummary();
       });
     });
 
@@ -115,6 +117,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       })
     })
   }
